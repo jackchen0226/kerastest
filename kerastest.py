d4f11e7cc302b6ccc7f2909af559378578bafc7a
Jack Chen <jackchen@wildhorn.ece.pdx.edu>
2016-12-29 15:09:17 -0800

    Clamp added, images of results present

--- kerastest.py
+++ kerastest.py
@@ -8,7 +8,7 @@ Gets to 98.40% test accuracy after 20 epochs
 from __future__ import print_function
 import numpy as np
 from scipy.misc.pilutil import imsave
-np.random.seed(9180)  # for reproducibility
+np.random.seed(413612)  # for reproducibility
 
 from keras.datasets import mnist
 from keras.models import Sequential, Model
@@ -21,10 +21,11 @@ from keras import backend as K
 
 batch_size = 128
 nb_classes = 10
-nb_epoch = 1
+nb_epoch = 20
 
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train
 
 X_train = X_train.reshape(60000, 784)
 X_test = X_test.reshape(10000, 784)
@@ -35,8 +36,6 @@ X_test /= 255
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
-#print(X_train)
-#imsave("startset.png", X_train)
 # convert class vectors to binary class matrices
 Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
@@ -74,24 +73,7 @@ class NoiseLayer(Layer):
         noise = K.random_normal(shape=K.shape(x),
                                       mean=0.,
                                       std=self.sigma)
-	# noise_x is the noise tensor combined with the input tensor
-        noise_x = x + noise        
-	# Then, noise_x is converted down to a list (tensor > numpy array > list)
-        noise_x = K.eval(noise_x)
-        print(noise_x)
-        noise_x = noise_x.tolist()
-        print(noise_x)
-	# Limit final values, a greyscale pixel can't be lighter than 1.0 (white) or darker than 0.0 (black)
-        for i in range(len(noise_x)):
-            for j in range(len(noise_x[i])):
-                if j > 1.0:
-                    noise_x[i][j] = 1.0
-                elif j < 0.0:
-                    noise_x[i][j] = 0.0
-
-        noise_x.asarray(noise_x)
-        y = K.placeholder(shape=(512,60000))
-        # Make sure noise_x becomes a tensor here
+        noise_x = K.clip(noise + x, 0.0, 1.0)
         return K.in_train_phase(noise_x, x)
 
     def get_config(self):
@@ -100,10 +82,8 @@ class NoiseLayer(Layer):
         return dict(list(base_config.items()) + list(config.items()))
 
 model = Sequential()
-model.add(GaussianNoise(3.0, input_shape=(784,)))
-#model.add(Dense(512, input_shape=(784,)))
+model.add(NoiseLayer(1.0, input_shape=(784,)))
 model.add(Dense(512))
-#model.add(NoiseLayer(1.0))
 model.add(Activation('relu'))
 model.add(Dropout(0.2))
 model.add(Dense(512, name="2nd_dense"))
@@ -127,8 +107,8 @@ print('Test score:', score[0])
 print('Test accuracy:', score[1])
 
 from datetime import datetime
-output_layer_name = "gaussiannoise_1"
+output_layer_name = "noiselayer_1"
 intermediate_layer_model = Model(input=model.input, output=model.get_layer(output_layer_name).output)
 intermediate_output = intermediate_layer_model.predict(X_train)
-print(intermediate_output)
+
 imsave('output_images/outfile {} {:%B %d,%H:%M:%S}.png'.format(output_layer_name, datetime.now()), intermediate_output)
